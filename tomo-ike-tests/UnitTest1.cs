3d70e83f0e4b470b28710c74002e774e37483d18
Matthew Eager <darthmajo@hotmail.com>
2026-01-14 20:52:53 +0100

    Unit Test Creation
    Moved the Godot project into a new subdirectory to create unit tests in
    the same repo.
    
    Going to use xUnit for these tests. Just created a new csproj for it
    with nothing in it at the moment.
    
    Feature #3

---
+++ tomo-ike-tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace tomo_ike_tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        
+    }
+}
---
+++ tomo-ike/src/level_generation/GodotMapScript.cs
@@ -0,0 +1,35 @@
+using Godot;
+using TomoIke;
+
+public partial class GodotMapScript : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+    {
+        MapGenerator mg = new MapGenerator(40, 22, 0.75);
+		mg.Generate(42);
+		Map m = mg.GeneratedMap;
+
+		TileMapLayer tm = (TileMapLayer)GetNode("/root/Level/Map/TileMapLayer");
+		buildTilemap(m, tm);
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	// Private Functions
+	private void buildTilemap(Map m, TileMapLayer tml)
+    {
+		for(int x = 0; x < m.MapSizeX; x++)
+        {
+            for(int y = 0; y < m.MapSizeY; y++)
+            {
+                int tileVal = (int)m.GetTile(x, y).Value;
+				tml.SetCell(new Vector2I(x,y), tileVal, atlasCoords: new Vector2I(0,0));
+            }
+        }
+    }
+
+}
